485f9ad19f35237ccd1cd746eef5aebe8b6aaa50
boboxoja9191 <boboxojabarnoyev010@gmail.com>
2025-05-18 19:36:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const javob = document.getElementById("javob");
 
 forma.addEventListener("submit", function (e) {
   e.preventDefault();
-  javob.textContent = forma.reset();
+  alert("Arizangiz qabul qilindi")
+  forma.reset()
 });
